16a3d11ca73d82642b9faabc090e98dc0a97f599
SteveK <54144390+Stephman1@users.noreply.github.com>
2024-12-01 14:54:29 +0000

    Quiz Questions API Route Connects to OpenAI

--- server/routes.js
+++ server/routes.js
@@ -10,8 +10,32 @@ const quiz_questions = async function(req, res) {
     const topic = req.params.topic.trim();
     const difficulty = req.params.difficulty;
 
-    res.json({});
+    api_key = config.openai_api_key;
 
+    const openai = new OpenAI({ apiKey: api_key });
+
+    const question = z.object({
+        question: z.string(),
+        answers: z.array(z.string()),
+        correctAnswer: z.number(),
+    });
+
+    const quiz = z.object({
+        questions: z.array(question),
+    });
+
+    const completion = await openai.beta.chat.completions.parse({
+        model: "gpt-4o-2024-08-06",
+        messages: [
+            { role: "system", content: "You are a quiz host." },
+            { role: "user", content: `Can I have 10 quiz questions each with 4 multiple choice answers on the topic of ${topic} at a difficulty level of ${difficulty}.`},
+        ],
+        response_format: zodResponseFormat(quiz, "quiz_questions"),
+    });
+
+    const quiz_questions = completion.choices[0].message.parsed;
+
+    res.json(quiz_questions);
 }
 
 module.exports = {
